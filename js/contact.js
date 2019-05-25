ed5f720a0236ebd178de28ca3a78a72e71951c49
akshat <akshatgoyalak23@gmail.com>
2019-05-25 13:29:50 +0530

    website made using js,html,css

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+$(window).bind("resize",function(){
+	if($(window).width()>930){
+		$(".top-right-section").show();
+	}else{
+		$(".top-right-section").hide();
+	}
+})
+
+$("#icon").click(function(){
+	$(".top-right-section").toggle();
+});
